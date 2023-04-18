412e961f7513aa2272ad13ba1dd4336ced7a0700
diegosg99 <diegosg22360@gmail.com>
2023-04-18 10:39:45 +0200

    cors

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ const formatComplete = 'YYYY-MM-DD HH:mm:ss'
 const PORT = 3003;
 const SECRET = "614f4f4a6568e9ae881c76e8753f65c9";
 
+const corsOptions = {
+  origin: "*", // Reemplaza "*" con el dominio o dominios permitidos en tus aplicaciones cliente
+  methods: "GET, POST, PUT, DELETE", // Métodos HTTP permitidos
+  allowedHeaders: "Content-Type, Authorization", // Encabezados permitidos
+};
+
 const app = express();
+
+const router = express.Router();
+
 app.set('port', process.env.PORT || PORT);
 
 app.use(express.json({limit: '500mb'}));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('dev'));
 
 
@@ -63,13 +72,93 @@ return result;
 
 //--------------------------------------- ROUTES -------------------------------------------
 
-app.use(require('./routes/admins.routes'));
+//app.use(require('./routes/admins.routes'));
 
-app.use(require('./routes/students.routes'));
+//app.use(require('./routes/students.routes'));
 
-app.use(require('./routes/courses.routes'));
+//app.use(require('./routes/courses.routes'));
 
-app.use(require('./routes/survey.routes'));
+router.get('/courses/name',(req,res) => {
+  try{
+      let sql = `SELECT code,name FROM cursos;`;
+      connection.query(sql, function(err, rows, fields) {
+          if (err) throw err;
+          res.status(200).send({rows});
+          });
+  }catch(error){
+      res.status(400).send({msg:"Error"});
+  }
+})
+
+router.get('/courses',(req,res) => {
+  try{
+      let sql = `SELECT * FROM cursos;`;
+      connection.query(sql, function(err, rows, fields) {
+          if (err) throw err;
+          res.status(200).send({rows});
+          });
+  }catch(error){
+      res.status(400).send({msg:"Error"});
+  }
+  })
+
+router.get('/course/documentation/:courseCode',(req,res) => {
+let courseCode = req.params.courseCode;
+try{
+    let sql = `SELECT documentationUrl FROM cursos WHERE code = '${courseCode}';`;
+    connection.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        res.status(200).send({rows});
+        });
+}catch(error){
+    res.status(400).send({msg:"Error"});
+}
+})
+
+router.get('/course/room/:courseCode',(req,res) => {
+let courseCode = req.params.courseCode;
+try{
+    let sql = `SELECT room FROM cursos WHERE code = '${courseCode}';`;
+    connection.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        res.status(200).send({rows});
+        });
+}catch(error){
+    res.status(400).send({msg:"Error"});
+}
+})
+
+router.get('/course/:courseCode',(req,res) => {
+let courseCode = req.params.courseCode;
+try{
+    let sql = `SELECT * FROM cursos WHERE code = '${courseCode}';`;
+    connection.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        res.status(200).send(rows[0]);
+        });
+}catch(error){
+    res.status(400).send({msg:"Error"});
+}
+})
+
+router.post('/courses/uploadExcel',(req,res) => {
+try{
+  let data = req.body;
+
+  data.forEach(item =>{
+    let sql = `INSERT IGNORE INTO cursos VALUES ('${item.id}','${item.code}','${item.name}','${item.tutor}','${item.room}','${item.day}','${item.documentation}')`;
+
+  connection.query(sql, function(err, rows, fields) {
+      if (err) throw err;
+      res.status(200).send("exito");
+      });
+  })
+}catch(error) {
+  res.status(400).send(req);
+}
+})
+
+//app.use(require('./routes/survey.routes'));
 
 //-------------------- Start Server ------------------------------------
 app.listen(app.get('port'), () =>
